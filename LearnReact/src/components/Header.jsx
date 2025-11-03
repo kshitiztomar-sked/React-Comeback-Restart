0f630bad53e6e8db7c4873ca60a9a1c5e89b382f
Kshitiz Tomar <kshitiz.tomar@skedgroup.in>
2025-11-03 19:16:32 +0530

    Header completed

--- LearnReact/src/components/Header.jsx
+++ LearnReact/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Header.css"
 
-export const Header = () => {
+export const Header = (props) => {
   return (
-    <div>Header</div>
+    <div>
+      <div className='nav'>
+        <div className='HeadingName'>
+          <h2>{props.Heading}</h2>
+        </div>
+        <div className='Navbar'>
+          <ul>
+            <li><a href=""></a>Home</li>
+            <li><a href=""></a>About us</li>
+            <li><a href=""></a>Learn</li>
+            <li><a href=""></a>Contact us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Header ;
